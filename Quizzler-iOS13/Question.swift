236288d7a4b99844d035eaa2782d6eef3af06225
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-09 23:26:05 +0400

    changed 2D array with struct component. added more questions to QUIZ.

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by ლუკა ზუხბაია on 8/9/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question {
+    let text: String
+    let answer: String
+    init(q: String,a: String){
+        self.text = q
+        self.answer = a
+    }
+    
+}
